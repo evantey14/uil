7ff0f2dbb562957bd203a392acb3c9e24900dc10
Beck Goodloe <beckgoodloe@lasacs.(none)>
2014-04-01 14:50:54 -0500

    removed duplicate exports in index.js

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,5 @@
 var passwordHash = require('password-hash');
 
-exports.index = function (req, res) {
-    res.render('index', {
-        title: 'LASA UIL Training'
-    });
-};
-
 exports.newuser = function (req, res) {
     res.render('newuser', {
         title: 'Add New User',
@@ -116,11 +110,11 @@ exports.signin = function (db) {
                 } else {
                     var hashed = found['password'];
                     if (passwordHash.verify(password, hashed)) {
-                        req.session = found["_id"];
+                        req.session = found['username'];
+                        req.session.id = found["_id"];
                         console.log(req.session);
                         req.session.user = found["username"];
                         console.log(req.session.user);
-                        res.location("/home");
                         res.redirect("/home");
                     } else {
                         res.render('login', {
@@ -138,14 +132,6 @@ exports.signin = function (db) {
             });
         }
     }
-};
-
-exports.home = function (req, res) {
-    var user = req.session.user;
-    console.log(user);
-    res.render('uniquelogin', {
-        title: "Welcome User!"
-    });
 };
 
 
@@ -181,28 +167,6 @@ exports.checkquestion = function (db) {
                         value: "incorrect"
                     });
                 }
-            }
-        });
-    }
-};
-
-
-exports.getquestion = function (db) {
-    return function (req, res) {
-        var collection = db.get('questions');
-        var thing = collection.find({}, function (err, found) {
-            if (err) {
-                throw err;
-            } else if (!found) {
-                res.render('error', {
-                    title: 'Error',
-                    prompt: 'We are having issues with the database. Sorry! \nPlease notify the creators and try again later.'
-                });
-            } else {
-                console.log(found);
-                var rand = Math.ceil(found.length * Math.random());
-                var id = found[rand]['_id'];
-                res.redirect('/random/' + id);
             }
         });
     }
@@ -256,106 +220,12 @@ exports.index = function (req, res) {
     });
 };
 
-exports.newuser = function (req, res) {
-    res.render('newuser', {
-        title: 'Add New User',
-        prompt: 'Please fill out the information below.',
-    });
-};
-
-exports.signin = function (db) {
-    return function (req, res) {
-        if (req.body.username != null) {
-            var username = req.body.username;
-            var password = req.body.password;
-            var collection = db.get("users");
-            collection.findOne({
-                "username": username
-            }, function (err, found) {
-                //console.log(found);
-                if (err) {
-                    throw err.$animate
-                }
-                if (!found) {
-                    res.render('login', {
-                        title: 'Login',
-                        prompt: 'Input your credentials below!',
-                        error: "username"
-                    });
-                } else {
-                    var hashed = found['password'];
-                    //console.log(hashed);
-                    if (passwordHash.verify(password, hashed)) {
-                        req.session = found['_id'];
-                        console.log(req.session.id);
-                        req.session.user = found['username'];
-                        console.log(req.session.user);
-                        res.location("/home");
-                        res.redirect("/home");
-                    } else {
-                        res.render('login', {
-                            title: 'Login',
-                            prompt: "Input your credentials below!",
-                            error: "matching"
-                        });
-                    }
-                }
-            });
-        } else {
-            res.render('login', {
-                title: 'Login',
-                prompt: 'Input your credentials below!'
-            });
-        }
-    }
-};
-
 exports.home = function (req, res) {
-    var user = req.session.user;
-    console.log(user);
+    res.send(req.session.id);
     res.render('uniquelogin', {
         title: "Welcome User!"
     });
 };
-
-
-exports.renderquestion = function (req, res) {
-    res.render('renderquestion', {
-        title: 'Random Question',
-        prompt: 'Please fill out the information below.',
-        question: 'question'
-    });
-};
-
-
-exports.checkquestion = function (db) {
-    return function (req, res) {
-        var choice = req.body.choice;
-        var id = req.body.id;
-        var collection = db.get('questions');
-        collection.findOne({
-            "_id": id
-        }, function (err, found) {
-            if (err) {
-                throw err;
-            } else {
-                var answer = found['key'];
-                if (choice == answer) {
-                    res.render('grading', {
-                        title: "CORRECT!",
-                        value: "correct"
-                    });
-                } else {
-                    res.render('grading', {
-                        title: "Incorrect...",
-                        value: "incorrect"
-                    });
-                }
-            }
-        });
-    }
-};
-
 
 exports.getquestion = function (db) {
     return function (req, res) {
@@ -373,46 +243,6 @@ exports.getquestion = function (db) {
                 var rand = Math.ceil(found.length * Math.random());
                 var id = found[rand]['_id'];
                 res.redirect('/random/' + id);
-            }
-        });
-    }
-};
-
-exports.viewquestion = function (db) {
-    return function (req, res) {
-        var id = req.params.id;
-        console.log(id);
-        var collection = db.get('questions');
-        var thing = collection.findOne({
-            '_id': id
-        }, function (err, found) {
-            if (err) {
-                throw err;
-            } else if (!found) {
-                res.render('error', {
-                    title: 'Error',
-                    prompt: 'We are having issues with the database. Sorry! \nPlease notify the creators and try again later.'
-                });
-            } else {
-                //console.log(found);
-                var title = 'Random Question';
-                var prompt = 'Test: ' + found['test'] + '\nQuestion: ' + found['ques'];
-                var answers = found['ans'];
-                console.log(answers);
-                res.render('renderquestion', {
-                    title: title,
-                    prompt: prompt,
-                    qnum: found['ques'],
-                    test: found['test'],
-                    question: found['text'],
-                    A: answers[0],
-                    B: answers[1],
-                    C: answers[2],
-                    D: answers[3],
-                    E: answers[4],
-                    id: found["_id"],
-                    url: found["_id"]
-                });
             }
         });
     }
